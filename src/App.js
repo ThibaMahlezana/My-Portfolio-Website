d6adb1c205ebe611b802bf23a7f18b489918ead5
ThibaMahlezana <thiba.ma@gmail.com>
2021-04-19 22:17:26 +0200

    Added about section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import Header from './Components/Header';
-import './styles.css'
+import React from "react";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import "./styles.css";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <Header />      
+      <Header />
+      <Home />
+      <About />
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,29 +1,50 @@
-import React from 'react'
+import React from "react";
 
-function Header (){
-    return(
-        <div>
-        <header className="header">
-            <div className="container">
-                <div className="row justify-content-between">
-                    <div className="logo"><a href="">T M</a></div>
-                    <div className="hamburger-btn outer-shadow hover-in-shadow">
-                        <span></span>
-                    </div>
-                </div>
+function Header() {
+  return (
+    <div>
+      <header className="header">
+        <div className="container">
+          <div className="row justify-content-between">
+            <div className="logo">
+              <a href="">T M</a>
             </div>
-            <nav className="nav-menu">
-                <div className="close-nav-menu">&times;</div>
-                <div className="nav-menu-inner">
-                    <ul>
-                        <li><a></a></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        
+            <div className="hamburger-btn outer-shadow hover-in-shadow">
+              <span></span>
+            </div>
+          </div>
         </div>
-    );
+        <nav className="nav-menu">
+          <div className="close-nav-menu outer-shadow hover-in-shadow">
+            &times;
+          </div>
+          <div className="nav-menu-inner">
+            <ul>
+              <li>
+                <a className="inner-shadow active">home</a>
+              </li>
+              <li>
+                <a className="outer-shadow hover-in-shadow">about</a>
+              </li>
+              <li>
+                <a className="outer-shadow hover-in-shadow">services</a>
+              </li>
+              <li>
+                <a className="outer-shadow hover-in-shadow">portfolio</a>
+              </li>
+              <li>
+                <a className="outer-shadow hover-in-shadow">testimonial</a>
+              </li>
+              <li>
+                <a className="outer-shadow hover-in-shadow">contact</a>
+              </li>
+            </ul>
+          </div>
+          <p className="copyright-text">&copy; 2021 Thiba Mahlezana</p>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
