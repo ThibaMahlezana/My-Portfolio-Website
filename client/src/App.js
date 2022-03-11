90240d871751cf30c02aaac0094cf12c66829309
ThibaMahlezana <thiba.ma@gmail.com>
2022-03-11 05:00:34 +0200

    Setting up files

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,10 @@
 import React from "react";
-import Header from "./Components/Header";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Service from "./Components/Service";
-import Portfolio from "./Components/Portfolio";
 import "./styles.css";
-
-import "./App.css";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Home />
-      <About />
-      <Service />
-      <Portfolio />
+      Portfolio
     </div>
   );
 }
