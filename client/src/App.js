12c456c732454eef5973b312ee57b53d922f85b3
ThibaMahlezana <thiba.ma@gmail.com>
2022-06-20 15:34:59 +0200

    Home section

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
@@ -8,6 +9,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div>
+      <Header />
       <Home />
       <About />
       <Portfolio />
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import '../styles/home.css';
+import bgImage from '../images/Pic3.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="row">
+        <div className='col-6 h-left'>
+          <p className="intro">Hello, I am </p>
+          <h1 className="name">Thiba Mahlezana</h1>
+          <h3 className="role">Full Stack Developer</h3>
+          <div className="h-btns">
+              <button variant="outline-primary" className="h-button">
+                <FontAwesomeIcon icon={faDownload} />
+                {' '} Download Resume
+              </button>
+              <button variant="outline-primary" className="h-button">
+                <FontAwesomeIcon icon={faEnvelope} />
+                {' '} Let's Talk
+              </button>
+          </div>
+        </div>
+        <div className='col-6 h-right'>
+          <div className="bg"></div>
+          <img src={bgImage}/>
+        </div>
+      </div>
+    </div>
   )
 }
