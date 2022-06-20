59dd475dea8492a2c0d3b385d5ace950eaf62e4f
ThibaMahlezana <thiba.ma@gmail.com>
2022-06-20 14:35:23 +0200

    Website structure setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      Portfolio
+      <Home />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
 }
