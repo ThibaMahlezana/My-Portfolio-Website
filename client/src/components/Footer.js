144ffba9ba55e5259eef538b2f704e41d4c573a0
ThibaMahlezana <thiba.ma@gmail.com>
2022-06-21 02:17:43 +0200

    Added footer section

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import '../styles/global.css'
+import '../styles/footer.css'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer class="footer">
+        <h2>Thiba Mahlezana</h2>
+        <p>&copy; Copyright 2022,  All rights reserved.</p>
+    </footer>
   )
 }
